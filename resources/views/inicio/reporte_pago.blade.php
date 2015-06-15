b590e9a94697d816769112e146baf91983f33438
FelixLeal <felix@softwarecriollo.com>
2015-06-14 19:39:51 -0430

    Lo subi para probar algo.

--- resources/views/inicio/reporte_pago.blade.php
+++ resources/views/inicio/reporte_pago.blade.php
@@ -6,29 +6,68 @@
 
 @section('contentInicio')
 
-	<div class="row" style="margin-top: 15px;">
-
-		<div class="small-11 large-6 large-centered columns hide-for-small-only" > 
-
-			<div class="row" style="margin-top: 20%;">
-
-				<div id="leyenda_2" style="font-size: 18px;">
-
-					<h3 style="font-weight: bold; color: #264A96;">Reporte pago!</h3>
-					Estas en la ventana principal de nuestra pagina,<br>
-					en donde podras realizar operaciones sencillas<br>
-					sobre tu perfil, gestionar proyectos y<br>
-					gestionar cotizaciones.<br>
-					<br><br>
-
-				</div>
-
-			</div>
-
-			<!--a href="{{ url('/auth/logout') }}" id="buttona"class="button large" style="text-transform: uppercase;">Aceptar</a-->
-
-		</div>
-
-	</div>
+    <div class="row panel" style="font-weight: bold; background-color: #ffffff;">
+        <div class="large-12 columns" style="pading: 0px"><h3>Reporte de Pago</h3></div>
+        <div class="large-8 columns" style="padding: 0px;">
+            <div class="large-6 columns" style="font-weight: bold !important;">
+                <strong>Nombre del cliente</strong>
+                <input type="text">
+            </div>
+            <div class="large-6 columns" style="font-weight: bold !important;">
+                <strong>Email</strong>
+                <input type="text">
+            </div>
+            <div class="large-6 columns" style="font-weight: bold !important;">
+                <strong>Forma de pago</strong><br>
+                <input type="radio" name="deposito" value="Deposito" id="dep"><label for="dep">Deposito</label>
+                <input type="radio" name="deposito" value="Transferencia" id="transf"><label for="transf">Transferencia</label>
+            </div>
+            <div class="large-6 columns" style="font-weight: bold !important;">
+                <strong>Nro de documento</strong>
+                <input type="text">
+            </div>
+            <div class="large-6 columns" style="font-weight: bold !important;">
+                <strong>Monto del pago</strong>
+                <input type="text">
+            </div>
+            <div class="large-6 columns" style="font-weight: bold !important;">
+                <strong>Banco destino</strong>
+                <div class="large-12 columns" style="padding: 0px;">
+                    <select>
+                        <option value=""></option>
+                        <option value=""></option>
+                    </select>
+                </div>
+            </div>
+            <div class="large-6 columns" style="font-weight: bold !important;">
+                <strong>Banco de Origen</strong>
+                <div class="large-12 columns" style="padding: 0px;">
+                    <select>
+                        <option value=""></option>
+                        <option value=""></option>
+                    </select>
+                </div>
+            </div>
+            <div class="large-6 columns" style="font-weight: bold !important;">
+                <strong>Cuenta de Origen(10 digitos)</strong>
+                <input type="text">
+            </div>
+            <div class="large-6 columns" style="font-weight: bold !important;">
+                <strong>Fecha de transaccion</strong>
+                <input type="text">
+            </div>
+            <div class="large-6 columns" style="font-weight: bold !important;">
+                <div class="large-12 columns" style="font-weight: bold !important; padding: 0px;">
+                    <a href="#" class="button large btn_crear" id="buttona" >Enviar</a>
+                </div>
+            </div>
+        </div>
+        <div class="large-4 columns" style="padding: 0px;">
+            <div class="large-12 columns" style="font-weight: bold !important;">
+                <strong>Comentario (opcional)</strong>
+                <textarea style="height: 200px;" placeholder="Escribir mensaje"></textarea>
+            </div>
+        </div>
+    </div>
 	
 @endsection
